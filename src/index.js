5bf6420e4b8bb73e4f50c0c96698fbc40c6b6f3c
Hamilton <prof.hamiltonmachiti@usjt.br>
2021-03-15 20:28:11 -0300

    criação do index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//importa os módulos http e express
+const http = require('http');
+const express = require('express');
+//constrói um objeto express
+const app = express();
+//importa o body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//configura a porta do servidor e o coloca em execução.
+const porta = 4000;
+app.set('port', porta);
+const server = http.createServer(app);
+server.listen(4000);
+
+app.get
