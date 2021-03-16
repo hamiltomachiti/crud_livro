bf781b9bd5ce3ffe5bde24d9a29ff0a99018cce4
Hamilton <prof.hamiltonmachiti@usjt.br>
2021-03-15 21:31:55 -0300

    Alteração no arquivo index.js (Inclusão do GET, POST,PUT e DELETE)

--- src/index.js
+++ src/index.js
@@ -12,4 +12,70 @@ app.set('port', porta);
 const server = http.createServer(app);
 server.listen(4000);
 
-app.get
+let id = 2;
+
+let livros = [{
+        id: 1,
+        titulo: "Lógica de Programação",
+        descricao: "Este livro ajudará os alunos com o conceito de lógica",
+        edicao: "3",
+        autor: "Hamilton Machiti da Costa",
+        isbn: "789123"
+    },
+    {
+        id: 2,
+        titulo: "Programação Web",
+        descricao: "Este livro ajudará os alunos com o conceito de web",
+        edicao: "2",
+        autor: "Arthur de Lima Costa",
+        isbn: "132654"
+    }
+];
+
+app.get("/livros", (req, res, next) => {
+    res.status(201).json(livros);
+})
+
+app.post("/livros", (req, res, next) => {
+    const livro = {
+        id: id += 1,
+        titulo: req.body.titulo,
+        descricao: req.body.descricao,
+        edicao: req.body.edicao,
+        autor: req.body.autor,
+        isbn: req.body.isbn
+    }
+    livros.push(livro)
+    res.status(201).json(livros);
+})
+
+app.put("/livros", (req, res, next) => {
+    livros.forEach((livro) => {
+        if (livro.id === req.body.id) {
+            livro.titulo = req.body.titulo,
+                livro.descricao = req.body.descricao,
+                livro.edicao = req.body.edicao,
+                livro.autor = req.body.autor,
+                livro.isbn = req.body.isbn
+        }
+    })
+    res.status(200).json(livros);
+})
+
+app.delete('/livros/:id', (req, res, next) => {
+    const idLivroDeletado = req.params.id;
+    livros.forEach((livro, index) => {
+        if (livro.id == idLivroDeletado) livros.splice(index, 1)
+    })
+    res.status(200).json(livros);
+})
+
+app.delete("/livros", (req, res, next) => {
+    livros.forEach((livro) => {
+        if (livro.id === req.body.id) {
+            const index = livros.indexOf(livro, 0)
+            livros.splice(index, 1)
+        }
+    })
+    res.status(200).json(livros);
+})
